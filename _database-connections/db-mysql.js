c875cd168c0f967d439c6a65810d79bf6e728f9f
Frankie Jeremy Bolanos <jeremybolanos@gmail.com>
2023-07-21 15:45:31 -0700

    first commit

---
+++ _database-connections/db-mysql.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "sampledb",
+});
+
+const sql = "SELECT * FROM users";
+
+con.connect(function (err) {
+  if (erro) throw err;
+  console.log("Connected!"); //- DEBUG
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    result.map((row) => {
+      console.log(row["name"]);
+    });
+  });
+});
